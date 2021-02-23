a7d51019ab042e2f93f6ae65c61c3fa328dc4b52
Shane Upchurch <shaneupchurch@gmail.com>
2021-02-23 13:43:14 -0600

    get route and find method working and test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Job = require('../lib/models/job');
 
 describe('jobAPPly-BE routes', () => {
   beforeEach(() => {
@@ -13,8 +14,8 @@ describe('jobAPPly-BE routes', () => {
       .post('/api/v1/jobs')
       .send({ 
         company: 'Imperfect Foods',
-        applied_date: '02/20/2021',
-        response_date: '',
+        appliedDate: '02/20/2021',
+        responseDate: '',
         url: 'www.imperfectfoods.com',
         notes: '' })
       .then(res => {
@@ -27,5 +28,31 @@ describe('jobAPPly-BE routes', () => {
           notes: ''
         });
       });
+  });
+
+  it('returns all jobs via GET', async() => {
+    const postLinks = await Promise.all([
+      { company: 'Imperfect Foods',
+        appliedDate: '02/20/2021',
+        responseDate: '',
+        url: 'www.imperfectfoods.com',
+        notes: '' },
+      { company: 'Alchemy',
+        appliedDate: '02/15/2021',
+        responseDate: '',
+        url: 'www.alchemycodelabs.com',
+        notes: '' },
+      { company: 'PayPal',
+        appliedDate: '02/22/2021',
+        responseDate: '',
+        url: 'www.paypal.com',
+        notes: '' }
+    ].map(post => Job.insert(post)));
+
+    const response = await request(app)
+      .get('/api/v1/jobs');
+
+    expect(response.body).toEqual(expect.arrayContaining(postLinks));
+    expect(response.body).toHaveLength(postLinks.length);
   });
 });
